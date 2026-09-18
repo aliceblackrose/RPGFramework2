f42ede18840b896f55d88ad7681fd5944b513b56
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-09-18 00:36:10 -0500

    Correct plugin bootstrap line breaks

--- src/main/java/io/github/math0898/rpgframework/RPGFramework.java
+++ src/main/java/io/github/math0898/rpgframework/RPGFramework.java
@@ -8,8 +8,10 @@ import io.github.math0898.rpgframework.commands.RpgCommand;
 import io.github.math0898.rpgframework.commands.StatsCommand;
 import io.github.math0898.rpgframework.damage.AdvancedDamageHandler;
 import io.github.math0898.rpgframework.items.ItemManager;
-import io.github.math0898.rpgframework.items.ItemRegistry;\nimport io.github.math0898.rpgframework.items.VanillaItemListener;
-import io.github.math0898.rpgframework.parties.PartyListener;\nimport io.github.math0898.rpgframework.parties.PartyManager;
+import io.github.math0898.rpgframework.items.ItemRegistry;
+import io.github.math0898.rpgframework.items.VanillaItemListener;
+import io.github.math0898.rpgframework.parties.PartyListener;
+import io.github.math0898.rpgframework.parties.PartyManager;
 import io.github.math0898.rpgframework.parties.PartyService;
 import io.github.math0898.rpgframework.player.CombatService;
 import io.github.math0898.rpgframework.player.PlayerLifecycleListener;
@@ -84,7 +86,8 @@ public final class RPGFramework extends JavaPlugin {
     private void registerListeners() {
         var manager = Bukkit.getPluginManager();
         manager.registerEvents(new PlayerLifecycleListener(playerService, combatService, cooldownService), this);
-        manager.registerEvents(new AdvancedDamageHandler(this, playerService, combatService), this);\n        manager.registerEvents(new VanillaItemListener(itemRegistry), this);
+        manager.registerEvents(new AdvancedDamageHandler(this, playerService, combatService), this);
+        manager.registerEvents(new VanillaItemListener(itemRegistry), this);
         manager.registerEvents(new PartyListener(this, partyService), this);
     }
 
